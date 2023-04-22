c3e58fb9fc742805420d8ba4e8ac1a51fa8458dd
shandianchengzi <shenshandian@hust.edu.cn>
2023-04-23 00:26:35 +0800

    add: user manuals.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # checkmd
 一个正在开发中的工具，去自动检测整个目录下所有 markdown 文档的正确性（包括错别字、失效链接等）
+
+## Usage
+
+1. 安装 python3 ，然后运行如下命令，安装依赖：
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+2. 克隆需要分析的仓库，修改 `main.py` 中的 `input_path` 变量，改成你的仓库的路径。
+
+3. 运行 `main.py` 程序即可：
+    ```bash
+    python3 main.py
+    ```
+
+## Developing
+- [x] 检查网页链接失效
+- [ ] 检查相对路径失效
+- [ ] 失效链接加上 User Agent 进一步确认是否失效
+- [ ] 错别字检查
+- [ ] 编码格式与文件实际保存格式的一致性检查
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import globs
 import os
 from extractLnk import extractLinkFromRepo
 
+input_path = "TencentOS-tiny"
+
 if __name__ == "__main__":
     # TODO: add args parse
     # TODO: add args[output_path] to set the output path by user.
@@ -9,4 +11,5 @@ if __name__ == "__main__":
     # TODO: delete all the output file safely.
     # os.system("rm %s/*.txt" % globs.log_path)
     # TODO: add arg[url_filter] to ignore some urls which have checked or no need to check.
-    ret = extractLinkFromRepo("TencentOS-tiny")
+    # TODO: add arg[input_path] to set the input path by user.
+    ret = extractLinkFromRepo(input_path)

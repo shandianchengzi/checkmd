ee414493ebaa7b4c32b1aca6fbe91434234090bc
shandianchengzi <shenshandian@hust.edu.cn>
2023-04-23 01:45:19 +0800

    todo: add 2 new todos.

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import globs
 import os
 from extractLnk import extractLinkFromRepo
 
-input_path = "TencentOS-tiny"
+input_path = "tke"
 
 if __name__ == "__main__":
     # TODO: add args parse
     # TODO: add args[output_path] to set the output path by user.
-    globs.log_path = "./output"
+    globs.log_path = "./%soutput"%input_path
+    if not os.path.exists(globs.log_path):
+        os.mkdir(globs.log_path)
     # TODO: delete all the output file safely.
     # os.system("rm %s/*.txt" % globs.log_path)
     # TODO: add arg[url_filter] to ignore some urls which have checked or no need to check.
